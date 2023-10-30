375a8aa02d1de4add914e285320850ccf5cdb40b
pushkarpawar15 <pushkarpawar2002@gmail.com>
2023-10-30 23:55:22 +0530

    "update"

--- calculator/src/main/java/calculator/App.java
+++ calculator/src/main/java/calculator/App.java
@@ -1,22 +1,36 @@
 package calculator;
-
 import java.util.Scanner;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    private static final Logger logger = LogManager.getLogger(App.class);
-    
+    // private static final Logger logger = LogManager.getLogger(App.class);
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static double divide(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+    }
     public static void main( String[] args )
     {
-        logger.info("Hello World");
-        System.out.println("Log4j version: " + org.apache.logging.log4j.util.PropertiesUtil.class.getPackage().getImplementationVersion());
-        Calculator calculator = new Calculator();
+        System.out.println("hello world");
+        // logger.info("Hello World");
+        // System.out.println("Log4j version: " + org.apache.logging.log4j.util.PropertiesUtil.class.getPackage().getImplementationVersion());
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Console Calculator!");
@@ -35,16 +49,15 @@ public class App
 
         switch (choice) {
             case 1:
-                System.out.println("Result: " + calculator.add(num1, num2));
+                System.out.println("Result: " + add(num1, num2));
                 break;
             case 2:
-                System.out.println("Result: " + calculator.subtract(num1, num2));
+                System.out.println("Result: " + subtract(num1, num2));
                 break;
             case 3:
-                System.out.println("Result: " + calculator.multiply(num1, num2));
-                break;
+                System.out.println("Result: " + multiply(num1, num2));
             case 4:
-                System.out.println("Result: " + calculator.divide(num1, num2));
+                System.out.println("Result: " + divide(num1, num2));
                 break;
             default:
                 System.out.println("Invalid choice. Please choose a valid operation.");
--- calculator/src/test/java/calculator/AppTest.java
+++ calculator/src/test/java/calculator/AppTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -17,5 +18,37 @@ public class AppTest
     {
         assertTrue( true );
     }
-    
+    @Test
+    public void testAdd() {
+        
+        int result = App.add(3, 5);
+        assertEquals(8, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        
+        int result = App.subtract(8, 3);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testMultiply() {
+        
+        int result = App.multiply(4, 6);
+        assertEquals(24, result);
+    }
+
+    @Test
+    public void testDivide() {
+        
+        double result = App.divide(10, 2);
+        assertEquals(5.0, result, 0.0001);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() {
+        
+        App.divide(10, 0);
+    }
 }
